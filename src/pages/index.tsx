109fdeeaae365d795b89bf944e6615f5a50948a1
Anishmehta8210 <anishmehta7782@gmail.com>
2023-08-20 23:45:45 +0530

    Banner/Cart

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,33 @@
 import Banner from "@/components/Banner";
+import { ProductProps } from "../../type";
+import Product from "@/components/Products";
 
 
+interface Props {
+  productData:ProductProps
+}
 
-
-export default function Home() {
+export default function Home({productData}: Props) {
+ 
   return (
     <>
     
     <main>
       <div className="max-w-screen-2xl mx-auto">
         <Banner/>
+        <div className="relative md:-mt020 lgl:-mt-32 xl:-mt-60 z-20mb-10">
+        <Product productData = {productData}/>
+
+        </div>
       </div>
     </main>
    
     </>
   )
 }
+
+export const getServerSideProps = async () => {
+  const res = await fetch("https://fakestoreapiserver.reactbd.com/tech");
+  const productData = await res.json();
+  return { props: { productData } };
+};
