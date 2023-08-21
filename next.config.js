f22cdbfb790903cd2f4dcba2b1a4c883c9bad1c5
Anishmehta8210 <anishmehta7782@gmail.com>
2023-08-22 02:23:59 +0530

    final day

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    remotePatterns: [
+      
+      { hostname: "lh3.googleusercontent.com" },
+    ],
     domains:["i.ibb.co"],
+    
   },
   reactStrictMode: true,
 }
