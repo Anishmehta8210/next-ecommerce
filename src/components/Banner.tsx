34060fce01aad6fe73f731d2d8b28cedb68b1274
Anishmehta8210 <anishmehta7782@gmail.com>
2023-08-21 23:34:44 +0530

    add to cart

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { Carousel } from "react-responsive-carousel";
 import sliderImg_1 from "../images/slider/sliderImg_1.jpg"
-import sliderImg_2 from "../images/slider/sliderImg_2.jpg"
+import sliderImg_2 from "../images/slider/sliderImg_4.jpg"
 import sliderImg_3 from "../images/slider/sliderImg_3.jpg"
 const Banner = () => {
   return (
